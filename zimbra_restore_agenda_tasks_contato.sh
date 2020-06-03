ba4ca1f371e4fff89584397d77c82b1c5b102e56
Charles Josiah Rusch Alandt <charles.alandt@gmail.com>
2020-06-02 22:40:47 -0300

    Update zimbra_restore_agenda_tasks_contato.sh

--- zimbra_restore_agenda_tasks_contato.sh
+++ zimbra_restore_agenda_tasks_contato.sh
@@ -5,24 +5,26 @@
 
 bkpCmd="zmmailbox"
 dirBkp="./bkp"
+dominio="dominio.com.br"
+
 
 
 for bkpCONTA in $(ls $dirBkp/*) ; do 
 
 	CONTA=`echo $bkpCONTA | awk -F'-' '{print $2}' | sed 's/.\{4\}$//'  ` 
-	echo -n "$CONTA - arq: $bkpCONTA -  " 
+	echo -n "$CONTA@$dominio - arq: $bkpCONTA -  " 
 	case $bkpCONTA in
 		*agenda* )
 			echo "Agenda..."
-			$bkpCmd -z -m $CONTA  pru /calendar?fmt=ics    $dirBkp/agenda-$CONTA.ics 
+			$bkpCmd -z -m $CONTA@$dominio  pru /calendar?fmt=ics    $dirBkp/agenda-$CONTA.ics 
 			;;
 		*tasks* )
 			echo "Tarefa..."
-			$bkpCmd -z -m $CONTA  pru /Tasks    $dirBkp/tasks-$CONTA.tsk
+			$bkpCmd -z -m $CONTA@$dominio  pru /Tasks    $dirBkp/tasks-$CONTA.tsk
 			;;
 		*contatos* )
 			echo "Contatos..."
-			$bkpCmd -z -m $CONTA  pru '/Contacts?fmt=zip&resolve=reset'  $dirBkp/contatos-$CONTA.zip 
+			$bkpCmd -z -m $CONTA@$dominio  pru '/Contacts?fmt=zip&resolve=reset'  $dirBkp/contatos-$CONTA.zip 
 			;;
 		*)
 			echo "Zicou"
