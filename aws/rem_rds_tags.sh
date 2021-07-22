4432d2489d46717fb45d7756a72430be7cbfa4fd
Charles Josiah Rusch Alandt <charles.alandt@gmail.com>
2021-07-22 20:58:19 -0300

    Create rem_rds_tags.sh
    first commit

---
+++ aws/rem_rds_tags.sh
@@ -0,0 +1,16 @@
+######
+# Description: Remove all AWS RDS TAGs on CSV format
+# Author: charles.alandt(a)gmail.com
+# Obs: Very ugly script... :D 
+#
+
+#!/bin/bash
+ARNS=$(aws rds describe-db-instances --query "DBInstances[].DBInstanceArn"  | jq -r ' .[]' | grep $1)
+for line in $ARNS; do
+    #NAME=$(aws rds describe-db-instances --query "DBInstances[?DBInstanceArn=='$line']" | jq -r  " .[] | .DBInstanceIdentifier ")
+    for TAGS in $(aws rds list-tags-for-resource --resource-name "$line" --query "TagList[]"  | jq  -r  '.[] | .Key   ')
+    do
+                    #echo "$line; $TAGS"  
+                    echo "aws rds remove-tags-from-resource --resource-name '$line' --tag-keys $TAGS"
+        done
+done
